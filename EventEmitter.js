ad35110f7e469de1b491cf60ff4f9f06f3ff9aa9
Danila Shutov <dcbrwn2@gmail.com>
2017-05-07 16:20:04 +0300

    Add EventListener implementation

---
+++ EventEmitter.js
@@ -0,0 +1,23 @@
+module.exports = class EventEmitter {
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(eventName, listener) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+
+    this.listeners[eventName].push(listener);
+  }
+
+  emit(eventName, params) {
+    if (!this.listeners[eventName]) {
+      return;
+    }
+
+    this.listeners[eventName].forEach((listener) => {
+      listener(params);
+    });
+  }
+}

474495daecd8ab6af4208ca289f8c44f12898b6a
Danila Shutov <dcbrwn2@gmail.com>
2017-05-07 22:47:13 +0300

    Add a song

---
+++ songs/humble.js
@@ -0,0 +1,107 @@
+window.songs = window.songs || {};
+
+window.songs.humble = [
+  {
+    "time": 0,
+    "type": "start"
+  },
+  {
+    "time": 0,
+    "type": "noteOn",
+    "note": 24
+  },
+  {
+    "time": 0,
+    "type": "noteOn",
+    "note": 0
+  },
+  {
+    "time": 400,
+    "type": "noteOn",
+    "note": 16
+  },
+  {
+    "time": 400,
+    "type": "noteOn",
+    "note": 7
+  },
+  {
+    "time": 400,
+    "type": "noteOn",
+    "note": 19
+  },
+  {
+    "time": 800,
+    "type": "noteOn",
+    "note": 0
+  },
+  {
+    "time": 1200,
+    "type": "noteOn",
+    "note": 12
+  },
+  {
+    "time": 1200,
+    "type": "noteOn",
+    "note": 16
+  },
+  {
+    "time": 1200,
+    "type": "noteOn",
+    "note": 7
+  },
+  {
+    "time": 1600,
+    "type": "noteOn",
+    "note": 0
+  },
+  {
+    "time": 2000,
+    "type": "noteOn",
+    "note": 12
+  },
+  {
+    "time": 2000,
+    "type": "noteOn",
+    "note": 7
+  },
+  {
+    "time": 2000,
+    "type": "noteOn",
+    "note": 24
+  },
+  {
+    "time": 2200,
+    "type": "noteOn",
+    "note": 23
+  },
+  {
+    "time": 2400,
+    "type": "noteOn",
+    "note": 0
+  },
+  {
+    "time": 2400,
+    "type": "noteOn",
+    "note": 24
+  },
+  {
+    "time": 2800,
+    "type": "noteOn",
+    "note": 7
+  },
+  {
+    "time": 2800,
+    "type": "noteOn",
+    "note": 24
+  },
+  {
+    "time": 3000,
+    "type": "noteOn",
+    "note": 23
+  },
+  {
+    "time": 3200,
+    "type": "end"
+  }
+]
